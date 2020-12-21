9bfa0702589aaa04cdb736493ec4f3ca20e7480a
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-12-21 16:36:53 +0100

    Using actions framework for logging

--- index.js
+++ index.js
@@ -2,23 +2,18 @@ const core = require('@actions/core');
 const init = require('./init');
 
 try {
-    const logging = core.getInput('logging')
     const environment = {};
 
     const outputs = init(environment);
 
     for (const property in outputs) {
         const value = outputs[property];
-        if (logging) {
-            console.log(`Output ${property} ==> ${value}`)
-        }
+        core.debug(`Output ${property} ==> ${value}`)
         core.setOutput(property, value);
     }
     for (const property in environment) {
         const value = environment[property];
-        if (logging) {
-            console.log(`Environment ${property} ==> ${value}`)
-        }
+        core.debug(`Environment ${property} ==> ${value}`)
         core.exportVariable(property, value);
     }
 } catch (error) {
